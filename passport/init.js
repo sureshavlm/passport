93181e9bc11f7ae0f46b843b8156d821d727d921
suresh <suresh.avlm@gmail.com>
2020-04-04 08:13:56 +0530

    passport authentication framework

---
+++ passport/init.js
@@ -0,0 +1,25 @@
+var login = require('./login'); //route to handle login strategy 
+var signup = require('./signup'); //route to handle registration logics
+var User = require('../models/user'); //ORM - user model mapped with mongo table
+
+module.exports = function(passport){
+
+	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function(user, done) {
+        console.log('serializing user: ');
+        console.log(user);
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            console.log('deserializing user:',user);
+            done(err, user);
+        });
+    });
+
+    // Setting up Passport Strategies for Login and SignUp/Registration
+    login(passport);
+    signup(passport);
+
+}
